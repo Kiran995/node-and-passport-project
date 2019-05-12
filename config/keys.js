4c11650aa4e50e4221378a9e704fe1f1e332746a
@kiranP <kiran.prajapati@yipl.com.np>
2019-05-12 21:42:06 +0545

    completed login, register route

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://kiran:l0v3@cluster0-5ssr3.mongodb.net/test?retryWrites=true'
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 router.get('/', (req, res) => res.render('welcome'));
+
+router.get('/dashboard', (req, ensureAuthenticated, res) => {
+    res.render('dashboard', {
+        name: req.user.name
+    })
+
+})
 
 module.exports = router;
